a539cff8d028f25a37af65f2e0f83003c7674515
OsborneSupremacy <osborne.ben@gmail.com>
2019-10-26 19:53:34 -0500

    initial checkin

---
+++ LaunchLocker.Library/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace LaunchLocker.Library
+{
+    public class Configuration
+    {
+        public string[] Args { get; set; }
+
+        public Configuration(string[] args)
+        {
+            Args = args;
+        }
+
+        public bool CheckIfValid(out string message)
+        {
+            message = string.Empty;
+
+            if (Args.Length == 0)
+            {
+                message = "At least one command line argument is required.";
+                return false;
+            };
+
+            string targetFileName = Args[0];
+
+            if(string.IsNullOrEmpty(targetFileName))
+            {
+                message = "The first command line argument should be the file to be lauched.";
+                return false;
+            }
+
+            var targetFileInfo = new FileInfo(targetFileName);
+
+            if(!targetFileInfo.Exists)
+            {
+                message = $"File `{targetFileInfo}` not found";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LaunchLocker.UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LaunchLocker.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Library.Configuration(args);
+            if (!config.CheckIfValid(out string message))
+                ExitWithMessage(message);
+
+        }
+
+        public static void ExitWithMessage(string Message)
+        {
+            Console.WriteLine(Message);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            Environment.Exit(-1);
+        }
+    }
+}
